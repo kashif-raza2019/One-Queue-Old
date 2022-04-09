ed77318dd09e79e4139376124907258c7a031264
Kashif Raza <kashifraza08012001@gmail.com>
2022-04-10 02:36:38 +0530

    Nodemon Installed

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@
 function selectChangeFunc() {
     var category = document.getElementById("category").value;
     var string = location.href;
+    var string1 = "";
+    var string2 = "";
     if(string.indexOf("?") > -1) {
         string1 = string.substring(0, string.indexOf("?"));
         string2 = string.substring(string.indexOf("?") + 1, string.length);

e6983655172b2ef2328096716a2e86d10923a2f8
Kashif Raza <kashifraza08012001@gmail.com>
2022-04-10 03:47:38 +0530

    Changes in Layout + Login

--- routes/home.js
+++ routes/home.js
@@ -6,12 +6,24 @@ router.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, '../' , 'views', 'index.html'));
 });
 
+router.use("/example", (req, res)=>{
+    res.sendFile(path.join(__dirname, '../' , 'views', 'Example' ,'example.html'));
+});
+
+router.use("/exampleverified", (req, res)=>{
+    res.sendFile(path.join(__dirname, '../' , 'views', 'Example' ,'exampletokenverified.html'));
+});
+
 router.use("/directorylist", (req, res)=>{
     res.sendFile(path.join(__dirname, '../' , 'views', 'dir.htm'));
 });
 
 router.use("/scanner", (req, res)=>{
     res.sendFile(path.join(__dirname, '../' , 'views', 'scanqr.html'));
+});
+
+router.use("/login", (req, res)=>{
+    res.sendFile(path.join(__dirname, '../' , 'views', 'loginform.html'));
 });
 
 module.exports = router;
--- routes/superadmin.js
+++ routes/superadmin.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use("/login", (req, res)=>{
-    res.send("Welcome to the Org Login page");
-});
 
 router.post("/dashboard", (req, res)=>{
     res.send("Welcome to the Govt. Dashboard page");
